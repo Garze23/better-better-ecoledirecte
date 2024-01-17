49963f1f72c5923daf02bef2797067a2fa869c00
Garze23 <149205373+Garze23@users.noreply.github.com>
2024-01-17 19:26:30 +0000

    h

---
+++ js/js_index.js
@@ -0,0 +1,12 @@
+<script>
+        window.OneSignalDeferred = window.OneSignalDeferred || [];
+        OneSignalDeferred.push(function (OneSignal) {
+            OneSignal.init({
+                appId: "07dd5e80-de35-46be-a122-c569f651f811",
+                safari_web_id: "web.onesignal.auto.2e92f9d5-12a5-4675-93ec-24ce5a91bfb8",
+                notifyButton: {
+                    enable: true,
+                },
+            })
+        })
+    </script>
